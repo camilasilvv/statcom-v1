45de876d35b1479b025dafee323de7f234b07395
Camila <camila.silvr@gmail.com>
2018-12-11 20:13:13 -0500

    Ajout des descriptions des fonctions

--- interface.py
+++ interface.py
@@ -2,7 +2,7 @@ import json
 import time
 
 # while True:
-#
+# test 1:
 # 	## Enregistrer les donnees recues du SDR sur la base de donnees:
 #
 # 	# pour le test on utilise une donnee rentree par l'usager.
@@ -42,23 +42,68 @@ import time
 # 	## Lecture du fichier latest pour affichage sur l'interface graphique
 # 	# peut etre cela se fera sur un autre programme. A voir
 
-data = {}
-data['people'] = []
-data['people'].append({
-    'name': 'Scott',
-    'website': 'stackabuse.com',
-    'from': 'Nebraska'
-})
-data['people'].append({
-    'name': 'Larry',
-    'website': 'google.com',
-    'from': 'Michigan'
-})
-data['people'].append({
-    'name': 'Tim',
-    'website': 'apple.com',
-    'from': 'Alabama'
-})
+# test 2:
+#data = {}
+#data['people'] = []
+#data['people'].append({
+#    'name': 'Scott',
+#    'website': 'stackabuse.com',
+#    'from': 'Nebraska'
+#})
+#data['people'].append({
+ #   'name': 'Larry',
+ #   'website': 'google.com',
+ #   'from': 'Michigan'
+#})
+#data['people'].append({
+#    'name': 'Tim',
+###    'website': 'apple.com',
+#    'from': 'Alabama'
+#})
+#
+#with open('data.json', 'w') as outfile:
+#    json.dump(data, outfile)
 
-with open('data.json', 'w') as outfile:
-    json.dump(data, outfile)
+def ConfigurationSDR():
+    # Cette fonction permet de configurer LimeSDR a partir des donnees entrees par l'usager
+
+def PositionAntenne():
+    # Cette fonction permet d'envoyer les parametres de suivi a l'Arduino pour le controle des moteurs
+
+def StoreData():
+    # Cette fonction permet d'enrgistrer les donnees recues dans la base de donnees, et de creer les fichiers de
+    # la base de donnees, le cas echeant
+
+def Receive():
+    # Cette fonction permet de recevoir les donnees decodees par LimeSDR et les enregistrer dans la base de donnees
+    # bloc provenant de LimeSDR
+    StoreData()
+
+def SendCommand():
+    # Cette fonction permet d'envoyer les commandes entrees par l'usager au SDR
+
+def ShowData():
+    # Cette fonction permet d'afficher les donnees les plus recentes des missions sur l'interface graphique.
+    # La fonction ouvre et lit les fichiers de donnees des deux dernieres semaines, et ensuite affiche la valeur la
+    # plus recentes de chaque varible
+    # a inserer bloc pour l'affichage dans l'interface
+
+def StreamCamera():
+    # Cette fonction permet d'initialiser la camera de surveillance, de recevoir et afficher la video en temps reel
+
+def GPredictMap():
+    # Cette fonction permet d'afficher la carte GPredict, bloc code open source
+
+def EntreeDonnesSDR():
+    # Cette fonction permet a l'usager d'entrer les parametres pour la configuration du SDR
+    # a regarder les parametres requis pour le suivi dans les bloc code de LimeSDR
+
+def TrackingTLE():
+    # Cette fonction permet a l'usager d'entrer les two-line element pour le suivi du satellite
+
+def EntreeCommands():
+    # Cette fonction permet a l'usager d'entrer les commandes a envoyer au satellite
+
+def ConfigurationInterface():
+    # Cette fonction permet de configurer l'environnement pour l'interface graphique
+    # a ajouter tous les blocs pour les entrees de commandes, parametres, etc
